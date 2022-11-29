e5ae2000dc724690ceafa2569ded218e68569262
PetrouilFan <pfanioudakis@gmail.com>
2022-11-29 13:10:45 +0200

    Fixed issue#3 where there was an error when building windows because pyinstaller was not in path

--- windows/build.cmd
+++ windows/build.cmd
@@ -1,5 +1,5 @@
 @echo off
-pip3 install pyinstaller
+python -m pip install pyinstaller
 mkdir .tmp
 cd .tmp
 cls
@@ -8,7 +8,7 @@ echo If you select yes you wont be able to add or remove connections and you'll 
 echo If you select no you'll need to have the data.yaml file in the same directory as the exe whenever you run it
 set /p include_yaml="(y/n): "
 if "%include_yaml%" == "y" (set build_parms=--add-data "../../data.yaml;.") else (set build_parms=)
-pyinstaller --noconfirm --onefile --console --icon "../src/icon.ico"  "../../main.py" %build_parms%
+python -m pyinstaller --noconfirm --onefile --console --icon "../src/icon.ico"  "../../main.py" %build_parms%
 xcopy /S /I /Q /Y /F "dist/main.exe" "../"
 cd ..
 @RMDIR /S /Q .tmp
